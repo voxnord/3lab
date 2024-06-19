de7701b8b7cb664f1a9aa482551ede6efebf16ea
knox <nikita.3.khorev@gmail.com>
2024-06-19 15:19:13 +0300

    Добавил работу кнопкам сохранения. открытия, закрытия, добавил выбор по типу файлов в открытии и сохранении, в сохранении файл сохраняет с изначальным значением

--- 3lab/3labForm.h
+++ 3lab/3labForm.h
@@ -19,9 +19,8 @@ namespace My3lab {
 		My3labForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
+
+			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &My3labForm::SaveQuit);
 		}
 
 	protected:
@@ -95,7 +94,6 @@ namespace My3lab {
 			// 
 			// menuStrip1
 			// 
-			this->menuStrip1->GripMargin = System::Windows::Forms::Padding(2, 2, 0, 2);
 			this->menuStrip1->ImageScalingSize = System::Drawing::Size(24, 24);
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
 				this->файлToolStripMenuItem,
@@ -103,7 +101,8 @@ namespace My3lab {
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(947, 33);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(4, 1, 0, 1);
+			this->menuStrip1->Size = System::Drawing::Size(631, 24);
 			this->menuStrip1->TabIndex = 2;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -114,34 +113,34 @@ namespace My3lab {
 					this->Save, this->SaveAs, this->Close
 			});
 			this->файлToolStripMenuItem->Name = L"файлToolStripMenuItem";
-			this->файлToolStripMenuItem->Size = System::Drawing::Size(69, 29);
+			this->файлToolStripMenuItem->Size = System::Drawing::Size(48, 22);
 			this->файлToolStripMenuItem->Text = L"Файл";
 			// 
 			// Open
 			// 
 			this->Open->Name = L"Open";
-			this->Open->Size = System::Drawing::Size(232, 34);
+			this->Open->Size = System::Drawing::Size(154, 22);
 			this->Open->Text = L"Открыть";
 			this->Open->Click += gcnew System::EventHandler(this, &My3labForm::Open_Click);
 			// 
 			// Save
 			// 
 			this->Save->Name = L"Save";
-			this->Save->Size = System::Drawing::Size(232, 34);
+			this->Save->Size = System::Drawing::Size(154, 22);
 			this->Save->Text = L"Сохранить";
 			this->Save->Click += gcnew System::EventHandler(this, &My3labForm::Save_Click);
 			// 
 			// SaveAs
 			// 
 			this->SaveAs->Name = L"SaveAs";
-			this->SaveAs->Size = System::Drawing::Size(232, 34);
+			this->SaveAs->Size = System::Drawing::Size(154, 22);
 			this->SaveAs->Text = L"Сохранить как";
 			this->SaveAs->Click += gcnew System::EventHandler(this, &My3labForm::SaveAs_Click);
 			// 
 			// Close
 			// 
 			this->Close->Name = L"Close";
-			this->Close->Size = System::Drawing::Size(232, 34);
+			this->Close->Size = System::Drawing::Size(154, 22);
 			this->Close->Text = L"Закрыть";
 			this->Close->Click += gcnew System::EventHandler(this, &My3labForm::Close_Click);
 			// 
@@ -152,43 +151,41 @@ namespace My3lab {
 					this->RepeatCount
 			});
 			this->дополнительноToolStripMenuItem->Name = L"дополнительноToolStripMenuItem";
-			this->дополнительноToolStripMenuItem->Size = System::Drawing::Size(156, 29);
+			this->дополнительноToolStripMenuItem->Size = System::Drawing::Size(107, 22);
 			this->дополнительноToolStripMenuItem->Text = L"Дополнительно";
 			// 
 			// PercentCount
 			// 
 			this->PercentCount->Name = L"PercentCount";
-			this->PercentCount->Size = System::Drawing::Size(229, 34);
+			this->PercentCount->Size = System::Drawing::Size(152, 22);
 			this->PercentCount->Text = L"Количество %";
 			this->PercentCount->Click += gcnew System::EventHandler(this, &My3labForm::PercentCount_Click);
 			// 
 			// RepeatCount
 			// 
 			this->RepeatCount->Name = L"RepeatCount";
-			this->RepeatCount->Size = System::Drawing::Size(229, 34);
+			this->RepeatCount->Size = System::Drawing::Size(152, 22);
 			this->RepeatCount->Text = L"Повторение";
 			this->RepeatCount->Click += gcnew System::EventHandler(this, &My3labForm::RepeatCount_Click);
 			// 
 			// TextBox
 			// 
-			this->TextBox->Location = System::Drawing::Point(12, 36);
+			this->TextBox->Location = System::Drawing::Point(8, 23);
+			this->TextBox->Margin = System::Windows::Forms::Padding(2);
 			this->TextBox->Multiline = true;
 			this->TextBox->Name = L"TextBox";
-			this->TextBox->Size = System::Drawing::Size(923, 577);
+			this->TextBox->Size = System::Drawing::Size(617, 376);
 			this->TextBox->TabIndex = 3;
-			/*// 
-			// openFileDialog При открытии окна проводника в поле ввода вводит текст в ""
-			// 
-			this->openFileDialog->FileName = L"Пасхалко";*/
 			// 
 			// My3labForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(947, 625);
+			this->ClientSize = System::Drawing::Size(631, 406);
 			this->Controls->Add(this->TextBox);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->Name = L"My3labForm";
 			this->ShowIcon = false;
 			this->Text = L"Lab 3 - Text Editor";
@@ -203,8 +200,8 @@ namespace My3lab {
 
 		void My3lab_Resize(System::Object^ sender, System::EventArgs^ e) // Масштабирование текстового поля
 		{
-			TextBox->Width = this->ClientSize.Width - 17;
-			TextBox->Height = this->ClientSize.Height - 32;
+			TextBox->Width = this->ClientSize.Width - 14;
+			TextBox->Height = this->ClientSize.Height - 30;
 		}
 
 		private: System::Void Open_Click(System::Object^ sender, System::EventArgs^ e) // Открытие файла
@@ -232,9 +229,15 @@ namespace My3lab {
 
 		private: System::Void SaveAs_Click(System::Object^ sender, System::EventArgs^ e) // Сохранение файла как
 		{
-			saveFileDialog->FileName = "TextFile"; // Изначальное имя для файла
-			saveFileDialog->DefaultExt = "txt"; // Расширение, в котором сохраняется файл
-			saveFileDialog->Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*|html file (*.html)|*.html|reg file (*.reg)|*.reg"; // Виды расширений, которые отображаются в окне
+			// Изначальное имя для файла
+			saveFileDialog->FileName = "TextFile"; 
+
+			// Расширение, в котором сохраняется файл
+			saveFileDialog->DefaultExt = "txt"; 
+
+			// Виды расширений, которые отображаются в окне
+			saveFileDialog->Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*|html file (*.html)|*.html|reg file (*.reg)|*.reg"; 
+
 			/*saveFileDialog->FilterIndex = 2;
 			saveFileDialog->RestoreDirectory = true;*/
 
@@ -246,13 +249,56 @@ namespace My3lab {
 
 		}
 
+		private: System::Void SaveQuit(System::Object^ sender, System::Windows::Forms::FormClosingEventArgs^ e) // Закрытие формы
+		{
+			System::Windows::Forms::DialogResult result;
+
+			if (!String::IsNullOrEmpty(this->TextBox->Text))
+			{
+				// Отображение сообщения с кнопками Yes и No
+				result = MessageBox::Show("Вы уверены, что хотите выйти?", "Выйти?", MessageBoxButtons::YesNo, MessageBoxIcon::Question); 
+				
+				// Проверка, была ли нажата кнопка Yes
+				if (result == System::Windows::Forms::DialogResult::Yes) 
+				{
+					result = MessageBox::Show("Вы хотите сохранить файл?", "Сохранить?", MessageBoxButtons::YesNo, MessageBoxIcon::Question);
+					
+					if (result == System::Windows::Forms::DialogResult::Yes)
+					{
+						Save_Click(sender, e); // Вызов функции сохранения
+					}
+					else
+					{
+						return;
+					}
+				}
+				
+				else
+				{
+					// Если нажата кнопка No, отменить закрытие формы
+					e->Cancel = true;
+				}
+			}
+		}
+
+
 		private: System::Void Close_Click(System::Object^ sender, System::EventArgs^ e) // Закрытие программы
 		{
-			Application::Exit();
+			
+			System::Windows::Forms::DialogResult result = MessageBox::Show("Вы уверены, что хотите выйти?", "Выйти?", MessageBoxButtons::YesNo, MessageBoxIcon::Question);
+
+			if (result == System::Windows::Forms::DialogResult::Yes)
+			{
+				Application::Exit();
+			}
+			
 		}
 
 		private: System::Void PercentCount_Click(System::Object^ sender, System::EventArgs^ e) // Подсчёт %
 		{
+			String^ pintext = this->TextBox->Text;
+			int count = 0;
+			char percent = '%';
 
 		}
 
